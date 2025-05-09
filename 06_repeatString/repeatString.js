2a0f49758ae1f70cd8d0c2e4a05e00ca16d7e358
Gabriel (Desktop) <106034246+gmjohnson683@users.noreply.github.com>
2025-05-08 21:14:15 -0500

    Pass last three tests for exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,5 @@
 const repeatString = function(string, number) {
+    if(number < 0) { return "ERROR"}
     let repeatedString = "";
     for(let i = 0; i < number; i++) {
         repeatedString += string;
