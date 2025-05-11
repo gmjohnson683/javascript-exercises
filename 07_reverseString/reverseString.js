d17b0cc4970425bafb4d3894bcbaa57ef6c705ed
Gabriel (Desktop) <106034246+gmjohnson683@users.noreply.github.com>
2025-05-11 16:58:31 -0500

    Write down pseudocode for solving exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,31 @@
 const reverseString = function() {
+// Line 1: The test file provides a word that needs to have its individual letters reversed.
+// (i.e., apple -> elppa)
+// Status:
 
+// Line 2: Create an empty array that will store all the individual letters of the given word.
+// (i.e., apple -> [a, p, p, l, e])
+// Status:
+
+// Code snippet
+
+// Line 3: The length of the word is stored in an variable, or data storage container. The data in this variable will used for the loop later in the code.
+// Status:
+
+// Code snippet
+
+// Line 4: The following loop will begin by taking apart the individual letters from the given word in a front-to-back manner, and store them in the empty array created earlier.
+// (i.e., apple -> [a, p, p, l, e])
+// Status: 
+
+// Code snippet
+
+// Line 5: The letters stored in the array will be glued together to form the reversed lettering of the given word at the beginning of the code. 
+// (i.e., [a, p, p, l, e] -> elppa)
+// Status: 
+
+
+// Code snippet
 };
 
 // Do not edit below this line
