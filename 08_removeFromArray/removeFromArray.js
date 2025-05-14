929559c975657d00090d89fa28d02a376d6445e1
Gabriel (Desktop) <106034246+gmjohnson683@users.noreply.github.com>
2025-05-13 21:24:42 -0500

    Detail a solution to the exercise in pseudocode

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
 const removeFromArray = function() {
+    // Line 1: The test file provides two inputs (one being an array with the other being a value to remove)
+    // Status:
+
+    // Line 2: Define a variable that will hold all the retained values from the modified array
+    // Status:
+
+    // Code snippet
+
+    // Line 3: Determine if the value that will be deleted in the original array
+    // Status:
+
+    // Code snippet
+
+    // Line 4: Delete the designated value (input given from the function) from the array and store the new array in the previously defined array
+    // Status:
+
+    // Code snippet
 };
 
 // Do not edit below this line
